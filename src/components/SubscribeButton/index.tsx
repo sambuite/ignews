adf27fa59483a1eb453c20a7cab373f05dd23765
sambuite <murilosambuite@gmail.com>
2022-02-28 21:06:13 -0300

    Added Stripe; Added SSG

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,12 @@
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({}: SubscribeButtonProps) {
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type='button' className={styles.subscribeButton}>
       Subscribe now
     </button>
   );
