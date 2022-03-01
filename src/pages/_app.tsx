54be7147301fd128be6031e99ed5fdb441cb1ce4
sambuite <murilosambuite@gmail.com>
2022-03-01 16:50:30 -0300

    Added API routes; Added authentication with Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import { AppProps } from "next/app";
-import { Header } from "../components/Header";
+import { SessionProvider as NextSessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 
-import "../styles/global.scss";
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextSessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextSessionProvider>
   );
 }
 
