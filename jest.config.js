b56453f8645af8912a601baeecc17792ea8f070b
sambuite <murilosambuite@gmail.com>
2022-04-27 01:53:07 -0300

    Added test coverage

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,11 @@ module.exports = {
   moduleNameMapper: {
     '\\.(scss|css|sass)': 'identity-obj-proxy',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/_app.tsx',
+    '!src/**/_document.tsx',
+  ],
+  coverageReporters: ['lcov', 'json'],
 };
