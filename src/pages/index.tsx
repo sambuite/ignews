afe51a4d13b5b73618486343060d5fb48232107d
sambuite <murilosambuite@gmail.com>
2022-04-26 23:56:24 -0300

    Added Home page tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
-
 import styles from './home.module.scss';
 
 interface HomeProps {
@@ -30,10 +29,10 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount} month.</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
-        <img src='/images/avatar.svg' alt='Girl coding' />
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
